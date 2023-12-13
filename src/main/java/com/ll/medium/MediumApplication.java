9a8aa6465b2d2f705887b3b57f9196d39b2e30cf
Park_Geyonggu <luckygoldsta@naver.com>
2023-12-13 14:12:40 +0900

    join move

--- src/main/java/com/ll/medium/MediumApplication.java
+++ src/main/java/com/ll/medium/MediumApplication.java
@@ -2,8 +2,10 @@ package com.ll.medium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //JPA Auditing 활성화
 public class MediumApplication {
 
     public static void main(String[] args) {
