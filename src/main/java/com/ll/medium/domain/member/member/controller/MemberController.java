ac999a9296aa4eec41122a466b750b728d7a2308
Park_Geyonggu <luckygoldsta@naver.com>
2023-12-14 15:22:11 +0900

    work

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.medium.domain.member.member.controller;
 
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -32,11 +33,12 @@ public class MemberController {
 
     }
     @PostMapping("/join")
-    public String signup(@Valid JoinForm joinForm) {
-        memberService.join(joinForm.getUsername(),joinForm.getPassword());
+    public String join(@Valid JoinForm joinForm) {
+        Member member = memberService.join(joinForm.getUsername(), joinForm.getPassword());
 
+        long id = member.getId();
 
-        return "redirect:/";
+        return "redirect:/?msg=No %d member joined.".formatted(id);
     }
 
 }
